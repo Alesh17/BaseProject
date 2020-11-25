c26376f4ae5d81203be6e3e6ba50bb5ab4054a58
alesh <d.aleshkov@vironit.co.uk>
2020-11-25 17:49:29 +0300

    added UseCase

--- domain/src/main/java/com/alesh/domain/usecase/UserUseCase.kt
+++ domain/src/main/java/com/alesh/domain/usecase/UserUseCase.kt
@@ -1,24 +1,24 @@
-package com.alesh.domain.interactor
+package com.alesh.domain.usecase
 
 import com.alesh.domain.common.constant.SortingConstants
 import com.alesh.domain.model.dto.User
 import com.alesh.domain.model.result.Result
-import com.alesh.domain.usecase.UserUseCase
+import com.alesh.domain.repository.UserRepository
 import javax.inject.Inject
 
-class UserInteractor @Inject constructor(
-    private val userUseCase: UserUseCase
+class UserUseCase @Inject constructor(
+    private val repository: UserRepository
 ) {
 
     suspend fun getUsers(): Result<List<User>> {
-        return userUseCase.getUsers()
+        return repository.getUsers()
     }
 
     suspend fun getUsersBySort(sort: SortingConstants): Result<List<User>> {
-        return userUseCase.getUsersBySort(sort)
+        return repository.getUsersBySort(sort)
     }
 
     suspend fun getUserById(id: Int): Result<User> {
-        return userUseCase.getUserById(id)
+        return repository.getUserById(id)
     }
 }
