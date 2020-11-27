06da134d8a5e20de9a2e00d5ec35f71c94f9bcb0
alesh <d.aleshkov@vironit.co.uk>
2020-11-27 11:46:10 +0300

    perf: change log msg to type Any

--- domain/src/main/java/com/alesh/domain/util/Logging.kt
+++ domain/src/main/java/com/alesh/domain/util/Logging.kt
@@ -3,7 +3,7 @@ package com.alesh.domain.util
 import android.os.SystemClock
 import android.util.Log
 
-fun log(tag: String = "", msg: String) = Log.v("Alesh! $tag", msg)
+fun log(tag: String = "", msg: Any) = Log.v("Alesh! $tag", msg.toString())
 
 inline fun <T> measureDuration(tag: String = "", block: () -> T): T {
     val start = SystemClock.uptimeMillis()
