eedbaac903621415a1ff28ec5ea4e1be5d19ad15
alesh <d.aleshkov@vironit.co.uk>
2021-03-02 21:03:24 +0300

    feat: add state save and restore in custom view

--- app/src/main/java/com/baseproject/widget/restrictedtv/RestrictedTextView.kt
+++ app/src/main/java/com/baseproject/widget/restrictedtv/RestrictedTextView.kt
@@ -2,6 +2,8 @@ package com.baseproject.widget.restrictedtv
 
 import android.content.Context
 import android.graphics.Canvas
+import android.os.Bundle
+import android.os.Parcelable
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatTextView
 import com.baseproject.R
@@ -33,5 +35,39 @@ class RestrictedTextView(
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         maxWidth = widthPixels.toInt()
+    }
+
+    /* State Save and Restore */
+
+    override fun onSaveInstanceState(): Parcelable {
+
+        val bundle = Bundle()
+        val textToSave = this.text.toString()
+        val superState = super.onSaveInstanceState()
+
+        val state = State(textToSave, superState)
+        bundle.putParcelable(State.STATE, state)
+
+        return bundle
+    }
+
+    override fun onRestoreInstanceState(instanceState: Parcelable?) {
+        if (instanceState is Bundle) {
+            val state = instanceState.getParcelable(State.STATE) as State? ?: return
+            this.text = state.text
+            super.onRestoreInstanceState(state.superState)
+            return
+        }
+        super.onRestoreInstanceState(BaseSavedState.EMPTY_STATE)
+    }
+
+    private class State(
+        val text: String,
+        superState: Parcelable?
+    ) : BaseSavedState(superState) {
+
+        companion object {
+            const val STATE = "RestrictedTextView.State"
+        }
     }
 }
