cc3de9d1c2f256ff9fcebf6441602e045fb22082
Dmitry Alesh <mr.assasin98@inbox.ru>
2021-05-15 15:48:00 +0300

    refactor: toast

--- app/src/main/java/com/baseproject/util/view/Toast.kt
+++ app/src/main/java/com/baseproject/util/view/Toast.kt
@@ -1,11 +1,12 @@
 package com.baseproject.util.view
 
 import android.content.Context
-import android.graphics.PorterDuff
 import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.StringRes
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.BlendModeColorFilterCompat.createBlendModeColorFilterCompat
+import androidx.core.graphics.BlendModeCompat
 import com.baseproject.R
 
 fun Context.toast(@StringRes messageStringRes: Int) {
@@ -15,7 +16,7 @@ fun Context.toast(@StringRes messageStringRes: Int) {
     val text: TextView = view.findViewById(android.R.id.message)
     val color = ContextCompat.getColor(this, R.color.colorPrimary)
 
-    view.background.setColorFilter(color, PorterDuff.Mode.SRC_IN)
+    view.background.colorFilter = createBlendModeColorFilterCompat(color, BlendModeCompat.SRC_ATOP)
     text.setTextColor(ContextCompat.getColor(this, android.R.color.white))
 
     toast.show()
