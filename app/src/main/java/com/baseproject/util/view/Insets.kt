bc865ab7ae5998c4e51ef4649806a24db050310f
Dmitry Alesh <mr.assasin98@inbox.ru>
2021-05-15 15:46:44 +0300

    refactor: insets

--- app/src/main/java/com/baseproject/util/view/Insets.kt
+++ app/src/main/java/com/baseproject/util/view/Insets.kt
@@ -1,25 +1,53 @@
 package com.baseproject.util.view
 
+import android.graphics.Rect
 import android.view.View
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePadding
 
 fun View.addSystemWindowInsetToPadding(
     left: Boolean = false,
     top: Boolean = false,
     right: Boolean = false,
-    bottom: Boolean = false
+    bottom: Boolean = false,
+    imeSensitive: Boolean = false
+) = doOnApplyWindowInsetsToPadding(imeSensitive) { view, systemWindowInsets, initialPadding ->
+    view.updatePadding(
+        left = initialPadding.left + (if (left) systemWindowInsets.left else 0),
+        top = initialPadding.top + (if (top) systemWindowInsets.top else 0),
+        right = initialPadding.right + (if (right) systemWindowInsets.right else 0),
+        bottom = initialPadding.bottom + (if (bottom) systemWindowInsets.bottom else 0)
+    )
+}
+
+fun View.addSystemWindowInsetToMargin(
+    left: Boolean = false,
+    top: Boolean = false,
+    right: Boolean = false,
+    bottom: Boolean = false,
+    imeSensitive: Boolean = false
+) = doOnApplyWindowInsetsToMargin(imeSensitive) { view, systemWindowInsets, initialMargin ->
+    view.updateMargin(
+        left = initialMargin.left + (if (left) systemWindowInsets.left else 0),
+        top = initialMargin.top + (if (top) systemWindowInsets.top else 0),
+        right = initialMargin.right + (if (right) systemWindowInsets.right else 0),
+        bottom = initialMargin.bottom + (if (bottom) systemWindowInsets.bottom else 0)
+    )
+}
+
+/** Adjust view insets via its padding */
+fun View.doOnApplyWindowInsetsToPadding(
+    imeSensitive: Boolean = false,
+    block: (View, Insets, Rect) -> Unit
 ) {
+
     val initialPadding = this.getPadding()
 
-    ViewCompat.setOnApplyWindowInsetsListener(this) { view, insets ->
-
-        view.updatePadding(
-            left = initialPadding.left + (if (left) insets.systemWindowInsets.left else 0),
-            top = initialPadding.top + (if (top) insets.systemWindowInsets.top else 0),
-            right = initialPadding.right + (if (right) insets.systemWindowInsets.right else 0),
-            bottom = initialPadding.bottom + (if (bottom) insets.systemWindowInsets.bottom else 0)
-        )
+    ViewCompat.setOnApplyWindowInsetsListener(this) { v, insets ->
+        val systemWindowInsets = insets.getSystemInsets(imeSensitive)
+        block(v, systemWindowInsets, initialPadding)
 
         insets
     }
@@ -27,22 +55,17 @@ fun View.addSystemWindowInsetToPadding(
     requestApplyInsetsWhenAttached()
 }
 
-fun View.addSystemWindowInsetToMargin(
-    left: Boolean = false,
-    top: Boolean = false,
-    right: Boolean = false,
-    bottom: Boolean = false
+/** Adjust view insets via its margin */
+fun View.doOnApplyWindowInsetsToMargin(
+    imeSensitive: Boolean = false,
+    block: (View, Insets, Rect) -> Unit
 ) {
+
     val initialMargin = this.getMargin()
 
-    ViewCompat.setOnApplyWindowInsetsListener(this) { view, insets ->
-
-        view.updateMargin(
-            left = initialMargin.left + (if (left) insets.systemWindowInsets.left else 0),
-            top = initialMargin.top + (if (top) insets.systemWindowInsets.top else 0),
-            right = initialMargin.right + (if (right) insets.systemWindowInsets.right else 0),
-            bottom = initialMargin.bottom + (if (bottom) insets.systemWindowInsets.bottom else 0)
-        )
+    ViewCompat.setOnApplyWindowInsetsListener(this) { v, insets ->
+        val systemWindowInsets = insets.getSystemInsets(imeSensitive)
+        block(v, systemWindowInsets, initialMargin)
 
         insets
     }
@@ -51,6 +74,12 @@ fun View.addSystemWindowInsetToMargin(
 }
 
 /* Internal */
+
+private fun WindowInsetsCompat.getSystemInsets(imeSensitive: Boolean = false): Insets {
+    @WindowInsetsCompat.Type.InsetsType var insetsTypeMask: Int = WindowInsetsCompat.Type.systemBars()
+    if (imeSensitive) insetsTypeMask = insetsTypeMask or WindowInsetsCompat.Type.ime()
+    return this.getInsets(insetsTypeMask)
+}
 
 private fun View.requestApplyInsetsWhenAttached() {
 
