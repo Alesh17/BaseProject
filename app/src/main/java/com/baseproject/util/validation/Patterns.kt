1e3a73c51dd71731e7382c54428dd6c786ba4f16
Dmitry Alesh <mr.assasin98@inbox.ru>
2021-05-15 15:46:16 +0300

    perf: add validation utils

---
+++ app/src/main/java/com/baseproject/util/validation/Patterns.kt
@@ -0,0 +1,8 @@
+package com.baseproject.util.validation
+
+const val EMAIL =
+    "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+            "\\@" + "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+            "(" + "\\." + "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" + ")+"
+
+const val PASSWORD_MIN = 6
---
+++ app/src/main/java/com/baseproject/util/validation/Validation.kt
@@ -0,0 +1,19 @@
+package com.baseproject.util.validation
+
+import com.baseproject.domain.error.ApplicationError
+import com.baseproject.domain.model.result.Result
+import com.baseproject.domain.model.result.toSuccess
+
+fun String.startCheck() = this.toSuccess()
+
+fun Result<String>.checkEmpty(): Result<String> {
+    return if (this is Result.Success) {
+        if (value.isNotEmpty()) Result.Success(value)
+        else Result.Error(ApplicationError.EmptyFiled)
+    } else this
+}
+
+private fun String.matchTo(regExp: String) = matches(regExp.toRegex())
+
+private fun String.isEmailValid() = this.matchTo(EMAIL)
+
